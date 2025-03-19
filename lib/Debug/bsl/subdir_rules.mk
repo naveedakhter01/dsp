bc93e0744d3752442cac24f372222d982f62095e
naveedakhter01 <50643178+naveedakhter01@users.noreply.github.com>
2025-03-19 13:11:16 +1300

    Add files via upload

---
+++ lib/Debug/bsl/subdir_rules.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+bsl/ezdsp5535.obj: ../bsl/ezdsp5535.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_gpio.obj: ../bsl/ezdsp5535_gpio.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_gpio.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_i2s.obj: ../bsl/ezdsp5535_i2s.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_i2s.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_lcd.obj: ../bsl/ezdsp5535_lcd.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_lcd.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_led.obj: ../bsl/ezdsp5535_led.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_led.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_sar.obj: ../bsl/ezdsp5535_sar.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_sar.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_sdcard.obj: ../bsl/ezdsp5535_sdcard.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_sdcard.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_spiflash.obj: ../bsl/ezdsp5535_spiflash.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_spiflash.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+bsl/ezdsp5535_uart.obj: ../bsl/ezdsp5535_uart.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C5500 Compiler'
+	"C:/ti/ccsv6/tools/compiler/c5500_4.4.1/bin/cl55" -v5515 -g --include_path="C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/c55xx_csl/inc" --include_path="D:/Projects/BatRecorder3_Ver_WIP/TI/BatRecorder/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="bsl/ezdsp5535_uart.pp" --obj_directory="bsl" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ lib/Debug/bsl/subdir_vars.mk
@@ -0,0 +1,72 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../bsl/ezdsp5535.c \
+../bsl/ezdsp5535_gpio.c \
+../bsl/ezdsp5535_i2s.c \
+../bsl/ezdsp5535_lcd.c \
+../bsl/ezdsp5535_led.c \
+../bsl/ezdsp5535_sar.c \
+../bsl/ezdsp5535_sdcard.c \
+../bsl/ezdsp5535_spiflash.c \
+../bsl/ezdsp5535_uart.c 
+
+OBJS += \
+./bsl/ezdsp5535.obj \
+./bsl/ezdsp5535_gpio.obj \
+./bsl/ezdsp5535_i2s.obj \
+./bsl/ezdsp5535_lcd.obj \
+./bsl/ezdsp5535_led.obj \
+./bsl/ezdsp5535_sar.obj \
+./bsl/ezdsp5535_sdcard.obj \
+./bsl/ezdsp5535_spiflash.obj \
+./bsl/ezdsp5535_uart.obj 
+
+C_DEPS += \
+./bsl/ezdsp5535.pp \
+./bsl/ezdsp5535_gpio.pp \
+./bsl/ezdsp5535_i2s.pp \
+./bsl/ezdsp5535_lcd.pp \
+./bsl/ezdsp5535_led.pp \
+./bsl/ezdsp5535_sar.pp \
+./bsl/ezdsp5535_sdcard.pp \
+./bsl/ezdsp5535_spiflash.pp \
+./bsl/ezdsp5535_uart.pp 
+
+C_DEPS__QUOTED += \
+"bsl\ezdsp5535.pp" \
+"bsl\ezdsp5535_gpio.pp" \
+"bsl\ezdsp5535_i2s.pp" \
+"bsl\ezdsp5535_lcd.pp" \
+"bsl\ezdsp5535_led.pp" \
+"bsl\ezdsp5535_sar.pp" \
+"bsl\ezdsp5535_sdcard.pp" \
+"bsl\ezdsp5535_spiflash.pp" \
+"bsl\ezdsp5535_uart.pp" 
+
+OBJS__QUOTED += \
+"bsl\ezdsp5535.obj" \
+"bsl\ezdsp5535_gpio.obj" \
+"bsl\ezdsp5535_i2s.obj" \
+"bsl\ezdsp5535_lcd.obj" \
+"bsl\ezdsp5535_led.obj" \
+"bsl\ezdsp5535_sar.obj" \
+"bsl\ezdsp5535_sdcard.obj" \
+"bsl\ezdsp5535_spiflash.obj" \
+"bsl\ezdsp5535_uart.obj" 
+
+C_SRCS__QUOTED += \
+"../bsl/ezdsp5535.c" \
+"../bsl/ezdsp5535_gpio.c" \
+"../bsl/ezdsp5535_i2s.c" \
+"../bsl/ezdsp5535_lcd.c" \
+"../bsl/ezdsp5535_led.c" \
+"../bsl/ezdsp5535_sar.c" \
+"../bsl/ezdsp5535_sdcard.c" \
+"../bsl/ezdsp5535_spiflash.c" \
+"../bsl/ezdsp5535_uart.c" 
+
+
