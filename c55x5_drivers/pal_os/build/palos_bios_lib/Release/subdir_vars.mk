85a3a06b9babbd5c3b8774ea7eb517649e653772
naveedakhter01 <50643178+naveedakhter01@users.noreply.github.com>
2025-03-19 13:17:39 +1300

    Add files via upload

---
+++ c55x5_drivers/pal_os/build/palos_bios_lib/Release/subdir_vars.mk
@@ -0,0 +1,60 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osCache.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osList.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osMem.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osprotect.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osSem.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osTime.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osWait.c 
+
+OBJS += \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/build/Release/pal_osCache.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/build/Release/pal_osList.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/build/Release/pal_osMem.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/build/Release/pal_osprotect.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/build/Release/pal_osSem.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/build/Release/pal_osTime.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/build/Release/pal_osWait.obj 
+
+C_DEPS += \
+./pal_osCache.pp \
+./pal_osList.pp \
+./pal_osMem.pp \
+./pal_osProtect.pp \
+./pal_osSem.pp \
+./pal_osTime.pp \
+./pal_osWait.pp 
+
+OBJS__QTD += \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_os\build\Release\pal_osCache.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_os\build\Release\pal_osList.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_os\build\Release\pal_osMem.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_os\build\Release\pal_osprotect.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_os\build\Release\pal_osSem.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_os\build\Release\pal_osTime.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_os\build\Release\pal_osWait.obj" 
+
+C_DEPS__QTD += \
+".\pal_osCache.pp" \
+".\pal_osList.pp" \
+".\pal_osMem.pp" \
+".\pal_osProtect.pp" \
+".\pal_osSem.pp" \
+".\pal_osTime.pp" \
+".\pal_osWait.pp" 
+
+C_SRCS_QUOTED += \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osCache.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osList.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osMem.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osprotect.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osSem.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osTime.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_os/src/bios/src/pal_osWait.c" 
+
+
---
+++ c55x5_drivers/pal_sys/build/pal_sys_bios_lib/Release/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_sys/da225/src/pal_sys.c 
+
+OBJS += \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_sys/build/Release/pal_sys.obj 
+
+C_DEPS += \
+./pal_sys.pp 
+
+OBJS__QTD += \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\pal_sys\build\Release\pal_sys.obj" 
+
+C_DEPS__QTD += \
+".\pal_sys.pp" 
+
+C_SRCS_QUOTED += \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/pal_sys/da225/src/pal_sys.c" 
+
+
