9601016acaafdf6997dedb732e02dfe5786a667b
naveedakhter01 <50643178+naveedakhter01@users.noreply.github.com>
2025-03-19 13:16:26 +1300

    Add files via upload

---
+++ c55x5_drivers/i2s/build/i2s_bios_drv_lib/Release/subdir_vars.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/src/dda_i2s.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/src/ddc_i2s.c \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/src/llc_i2s.c 
+
+OBJS += \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/build/Release/dda_i2s.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/build/Release/ddc_i2s.obj \
+C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/build/Release/llc_i2s.obj 
+
+C_DEPS += \
+./dda_i2s.pp \
+./ddc_i2s.pp \
+./llc_i2s.pp 
+
+OBJS__QTD += \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\i2s\build\Release\dda_i2s.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\i2s\build\Release\ddc_i2s.obj" \
+"C:\projects\builds\c55_caf\config_17\git\c55_caf\c55x5_drivers\i2s\build\Release\llc_i2s.obj" 
+
+C_DEPS__QTD += \
+".\dda_i2s.pp" \
+".\ddc_i2s.pp" \
+".\llc_i2s.pp" 
+
+C_SRCS_QUOTED += \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/src/dda_i2s.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/src/ddc_i2s.c" \
+"C:/projects/builds/c55_caf/config_17/git/c55_caf/c55x5_drivers/i2s/src/llc_i2s.c" 
+
+
